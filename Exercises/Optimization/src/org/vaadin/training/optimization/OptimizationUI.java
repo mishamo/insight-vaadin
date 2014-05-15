e4314e00a397373df2026a0c2663bd454bcd365d
Misha Griffiths <mishamo@gmail.com>
2014-05-15 10:22:32 +0100

    Optimisations

--- Exercises/Optimization/src/org/vaadin/training/optimization/OptimizationUI.java
+++ Exercises/Optimization/src/org/vaadin/training/optimization/OptimizationUI.java
@@ -1,53 +1,41 @@
 package org.vaadin.training.optimization;
 
+import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+@Theme("mytheme")
 public class OptimizationUI extends UI {
 
 	@Override
 	public void init(VaadinRequest request) {
-		VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
+		CssLayout layout = new CssLayout();
+        layout.setStyleName("vertical");
 
 		for (int i = 0; i < 100; i++) {
-			Panel personPanel = new Panel();
-			personPanel.setCaption("John Doe");
-
-			layout.addComponent(personPanel);
-
-			HorizontalLayout hl = new HorizontalLayout();
-			hl.setMargin(true);
-			personPanel.setContent(hl);
+			CssLayout hl = new CssLayout();
+            hl.setStyleName("details");
+            layout.addComponent(hl);
+            hl.setCaption("John Doe");
 			hl.setWidth("100%");
 
-			VerticalLayout contactDetails = new VerticalLayout();
-			Label phone = new Label();
-			phone.setValue("Phone: 123 4567");
-			contactDetails.addComponent(phone);
-			Label fax = new Label();
-			fax.setValue("Fax: 123 4568");
-			contactDetails.addComponent(fax);
-			Label email = new Label();
-			email.setValue("Email: example@example.com");
-			contactDetails.addComponent(email);
-			hl.addComponent(contactDetails);
+            Label details = new Label("Phone: 123 4567<br/>Fax: 123 4568<br/>Email: example@example.com");
+            details.setContentMode(ContentMode.HTML);
+            details.setWidth(50, Unit.PERCENTAGE);
+			hl.addComponent(details);
 
-			VerticalLayout addressLayout = new VerticalLayout();
-			Label address = new Label();
-			address.setValue("Random street 1");
-			addressLayout.addComponent(address);
-			Label city = new Label();
-			city.setValue("City of Lorem Ipsum");
-			addressLayout.addComponent(city);
-			Label zip = new Label();
-			zip.setValue("012345");
-			addressLayout.addComponent(zip);
-			hl.addComponent(addressLayout);
+            Label addressLabel = new Label("Random street 1<br/>City of Lorem Ipsum<br/>012345");
+            addressLabel.setContentMode(ContentMode.HTML);
+            addressLabel.setWidth(50, Unit.PERCENTAGE);
+
+			hl.addComponent(addressLabel);
 
 		}
 
